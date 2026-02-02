b62ad46f774f2b278c52a86ccc698166d31ac782
OKUMUKOKUMU <143013490+OKUMUKOKUMU@users.noreply.github.com>
2026-02-02 09:25:57 +0300

    Create firebase config with project details
    Add Firebase configuration for the project

---
+++ src/firebase/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  "projectId": "studio-618122639-36981",
+  "appId": "1:812026703414:web:e33b24ef9422147ae57297",
+  "apiKey": "AIzaSyBLkNUz-mrDhsnNAi8jnd8MhNa0NApEQrc",
+  "authDomain": "studio-618122639-36981.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "812026703414"
+};
+
